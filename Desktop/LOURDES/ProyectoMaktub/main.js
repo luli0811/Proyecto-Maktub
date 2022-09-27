ce76351cf6fe82b2b9e446a6f12398a07edbcf8a
Lourdes Sanchez <luli.sanchez0811@gmail.com>
2022-09-27 19:04:52 -0300

    Actualización clases

--- Desktop/LOURDES/ProyectoMaktub/main.js
+++ Desktop/LOURDES/ProyectoMaktub/main.js
@@ -1,5 +1,27 @@
 let resultado = 0;
 let resultadoAux = 0;
+class cliente {
+    constructor(nombre, edad, direccion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+}
+class Producto {
+    constructor(nombre, precio, id) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+    }
+    mostrarPrecio() {
+        alert("El producto " + this.nombre + " cuesta: $" + this.precio);
+    }
+}
+const Producto1 = new Producto("Agenda", 4500, "001");
+const Producto2 = new Producto("Lapicera", 100, "002");
+const Producto3 = new Producto("Cuaderno", 1500, "003");
+
 
 //Funcion calculo por producto.
 function calcular(art) {
@@ -7,19 +29,19 @@ function calcular(art) {
     let resultadocalcular;
     switch (art) {
         case "Agenda":
-            alert("Las agendas cuestan $4500");
+            Producto1.mostrarPrecio();
             cantidad = prompt("Ingrese la cantidad que desea");
-            resultadocalcular = parseInt(cantidad) * 4500;
+            resultadocalcular = parseInt(cantidad) * Producto1.precio;
             break;
         case "Lapicera":
-            alert("Las lapiceras cuestan $100");
+            Producto2.mostrarPrecio();
             cantidad = prompt("Ingrese la cantidad que desea");
-            resultadocalcular = parseInt(cantidad) * 100;
+            resultadocalcular = parseInt(cantidad) * Producto2.precio;
             break;
         case "Cuaderno":
-            alert("Los cuadernos cuestan $1500");
+            Producto3.mostrarPrecio();
             cantidad = prompt("Ingrese la cantidad que desea");
-            resultadocalcular = parseInt(cantidad) * 1500;
+            resultadocalcular = parseInt(cantidad) * Producto3.precio;
             break;
         default:
             resultado = 0;
@@ -34,9 +56,10 @@ function suma(numeroA, numeroB) {
 }
 
 //Variables usadas
-let NombreCliente = prompt("¿Cómo te llamas?");
-alert("Hola " + NombreCliente + "!");
-alert("En cual de los artículos estás interesado " + NombreCliente + "?" + "\n" + "Podés elegir: " + "\n" + "-Agenda\n- Cuaderno\n - Lapicera");
+const cliente1 = new cliente(prompt("¿Cómo te llamas?"), prompt("¿Que edad tenés?"), prompt("¿Dirección de envio?"));
+console.log(cliente1);
+alert("Hola " + cliente1.nombre + "!");
+alert("En cual de los artículos estás interesado " + cliente1.nombre + "?" + "\n" + "Podés elegir: " + "\n" + "-Agenda\n -Cuaderno\n -Lapicera");
 let Articulo = prompt("Ingrese uno de los artículos mencionados");
 resultadoAux = calcular(Articulo);
 resultado = suma(resultadoAux, resultado);
@@ -48,7 +71,7 @@ while (condicion == "OK") {
     resultadoAux = calcular(Articulo);
     resultado = suma(resultadoAux, resultado);
     alert("Hasta acá llevas : " + resultado);
-    condicion = prompt("Si quiere seleccionar otro articulo escriba la palabra OK\n. Si no presione un caracter aleatorio");
+    condicion = prompt("Si quiere seleccionar otro articulo escriba la palabra OK.\n Si no presione un caracter aleatorio");
 }
 alert("El total de la compra es: " + resultado);
 alert("Muchas gracias por tu compra" + NombreCliente + ",te esperamos!");
